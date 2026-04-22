99fab38b6385ebfc1ba1fcceccb16441babc79db
Jovialngandu <jovialngandu2@gmail.com>
2026-04-23 00:54:29 +0100

    [UPDATE] Delegations des UserInteraction  requetes aux UserInteraction  repositories dans les endpoints

--- app/api/v1/endpoints/interactions.py
+++ app/api/v1/endpoints/interactions.py
@@ -1,23 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.dependencies import get_db, get_current_user
-from app.models.models import UserInteraction
 from app.schemas.interaction import InteractionSchema, InteractionUpdate
+from app.repositories import interaction_repository
 
 router = APIRouter()
-
-def get_or_create_interaction(db: Session, user_id: int, article_id: int):
-    interaction = db.query(UserInteraction).filter(
-        UserInteraction.article_id == article_id,
-        UserInteraction.user_id == user_id
-    ).first()
-    
-    if not interaction:
-        interaction = UserInteraction(user_id=user_id, article_id=article_id)
-        db.add(interaction)
-        db.commit()
-        db.refresh(interaction)
-    return interaction
 
 @router.patch("/{article_id}", response_model=InteractionSchema)
 def update_interaction(
@@ -26,17 +13,13 @@ def update_interaction(
     db: Session = Depends(get_db), 
     current_user = Depends(get_current_user)
 ):
-    interaction = get_or_create_interaction(db, current_user["id"], article_id)
-    
-    # Mise à jour dynamique des champs fournis
+    # On transforme le schema en dict propre
     update_data = data.model_dump(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(interaction, key, value)
-        
-    db.commit()
-    db.refresh(interaction)
-    return interaction
+    
+    # On délègue au repository
+    return interaction_repository.update(db, current_user["id"], article_id, update_data)
 
 @router.get("/", response_model=list[InteractionSchema])
 def get_all_my_interactions(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    return db.query(UserInteraction).filter(UserInteraction.user_id == current_user["id"]).all()
+    # On délègue au repository
+    return interaction_repository.get_by_user(db, current_user["id"])
