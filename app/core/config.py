c3ccf4b61712659905f8d6c3e1baeb9616ae67e4
Jovialngandu <jovialngandu2@gmail.com>
2026-04-23 03:54:02 +0100

    [UDPATE] Adaptation de la config pour l'auth

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str = Field(default="sqlite:///./app_data.db")
     GNEWS_API_KEY: str = Field(..., env="GNEWS_API_KEY")
     GNEWS_BASE_URL: str = Field(default="https://gnews.io/api/v4/top-headlines")
+    SECRET_KEY: str = Field(..., env="SECRET_KEY")
+    ALGORITHM: str = Field(default="HS256", env="ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=30, env="ACCESS_TOKEN_EXPIRE_MINUTES")
 
     model_config = SettingsConfigDict(
         env_file=".env", 
